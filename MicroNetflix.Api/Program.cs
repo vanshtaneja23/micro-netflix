ae4b40334837b8a7d0c39d9392e4bf8674aa77af
vanshtaneja23 <155813476+vanshtaneja23@users.noreply.github.com>
2025-12-15 23:01:40 -0700

    Api: Configure DI for DbContext, MassTransit, and MinIO

--- MicroNetflix.Api/Program.cs
+++ MicroNetflix.Api/Program.cs
@@ -6,7 +6,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database
+builder.Services.AddDbContext<VideoDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Host=localhost;Database=micronetflix;Username=admin;Password=password"));
+
+// Message Bus
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+
+// Storage
+builder.Services.AddMinio(configureClient => configureClient
+    .WithEndpoint("localhost", 9000)
+    .WithCredentials("minioadmin", "minioadmin")
+    .WithSSL(false)
+    .Build());
+
 var app = builder.Build();
+
+// Ensure DB Created
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<VideoDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
