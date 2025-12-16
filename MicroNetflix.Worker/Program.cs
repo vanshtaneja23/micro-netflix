8a8aaff2ae0d5efb501f0be7d6c5aeebd2fd1832
vanshtaneja23 <155813476+vanshtaneja23@users.noreply.github.com>
2025-12-15 23:02:47 -0700

    Worker: Configure MassTransit and MinIO

--- MicroNetflix.Worker/Program.cs
+++ MicroNetflix.Worker/Program.cs
@@ -1,6 +1,36 @@
+using MassTransit;
 using MicroNetflix.Worker;
+using Minio;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Storage
+builder.Services.AddMinio(configureClient => configureClient
+    .WithEndpoint("localhost", 9000)
+    .WithCredentials("minioadmin", "minioadmin")
+    .WithSSL(false)
+    .Build());
+
+// Message Bus
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<VideoUploadedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ReceiveEndpoint("video-processing-queue", e =>
+        {
+            e.ConfigureConsumer<VideoUploadedConsumer>(context);
+        });
+    });
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
